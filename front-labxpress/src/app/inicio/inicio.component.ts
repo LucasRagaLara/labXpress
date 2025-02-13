065db1696f19810242957bdc77bc7649ff8fecae
lucasraga <lucasraga29@gmail.com>
2025-02-13 15:27:32 +0100

    Update de inicio, arreglos de menu e interacciones

--- front-labxpress/src/app/inicio/inicio.component.ts
+++ front-labxpress/src/app/inicio/inicio.component.ts
@@ -1,79 +1,99 @@
-import { Component } from '@angular/core';
+import { OnInit, Component } from '@angular/core';
+import { Router } from '@angular/router';
 import { MenuComponent } from "../menu/menu.component";
 import { CarouselModule } from 'primeng/carousel';
 import { ButtonModule } from 'primeng/button';
-import { Tag, TagModule } from 'primeng/tag';
-import { RouterModule } from '@angular/router';
+import { TagModule } from 'primeng/tag';
+
 import { CommonModule } from '@angular/common';
+
 @Component({
   selector: 'app-inicio',
+  standalone: true, 
   imports: [
     MenuComponent, 
     CarouselModule,
     ButtonModule,
     TagModule,
-    RouterModule,
     CommonModule
   ],
   templateUrl: './inicio.component.html',
-  styleUrl: './inicio.component.sass'
+  styleUrls: ['./inicio.component.sass'],
 })
-export class InicioComponent {
-  images = [
-    {
-      contentType: "carousel1",
-      src: 'images/carousel/furgoneta_carousel.png',
-      alt: 'Furgoneta movil',
-      title: 'Resultats ràpids, salut sense espera',
-      description: 'La nostra furgoneta mòbil porta les proves mèdiques a la teva porta, oferint un servei ràpid, còmode i segur. Amb tecnologia davantguarda i professionals especialitzats, garantim resultats fiables sense que hagis de desplaçar-te. La teva salut, sense esperes!',
-      actionText: 'Coneix els nostres serveis',
-      link: '/servicios'
-    },
-    {
-      contentType: "carousel2",
-      src: 'images/carousel/furgoneta-interior-min.jpg',
-      alt: 'App mòbil',
-      title: 'Gestió fàcil a l\'app',
-      description: 'Consulta els resultats i gestiona les cites des del teu mòbil.',
-      actionText: 'Descarrega l\'app',
-      link: '/app'
-    },
-    {
-      contentType: "carousel3",
-      src: 'images/carousel/Merchandasing-min.png',
-      alt: "T'agrada el merchandising?",
-      title: "T'agrada el merchandising?",
-      description: '"Ràpid, còmode i segur. Em va encantar el servei!"',
-      actionText: 'Llegeix més testimoni',
-      link: '/testimonis'
-    },
-    {
-      contentType: "carousel4",
-      src: 'images/carousel/Folleto-min.png',
-      alt: 'Oferta especial',
-      title: "Tens algún Dubte?",
-      description: 'Aprofita les nostres promocions en proves mèdiques.',
-      actionText: 'Més informació',
-      link: '/contacto'
-    }
-  ];
+export class InicioComponent implements OnInit {
 
-  responsiveOptions = [
-    {
-      breakpoint: '1024px',
-      numVisible: 2,
-      numScroll: 1
-    },
-    {
-      breakpoint: '600px',
-      numVisible: 1,
-      numScroll: 1
-    }
-  ];
+  images: any[] | undefined;
+  responsiveOptions: any[] | undefined;
+
+  constructor(private router: Router) { }
+
+  ngOnInit() {
+    this.images = [
+      {
+        contentType: "carousel1",
+        src: 'images/carousel/furgoneta_carousel.png',
+        alt: 'Furgoneta movil',
+        title: 'Resultats ràpids, salut sense espera',
+        description: 'La nostra furgoneta mòbil porta les proves mèdiques a la teva porta...',
+        icono1: "images/carousel/carousel1/furgoneta-de-reparto.png",
+        icono2: "images/carousel/carousel1/casa.png",
+        icono3: "images/carousel/carousel1/microbiologia.png",
+        actionText: 'Coneix els nostres serveis',
+        link: '/servicios'
+      },
+      {
+        contentType: "carousel2",
+        src: 'images/carousel/furgoneta-interior-min.jpg',
+        alt: 'App mòbil',
+        title: "Estem a l'última!",
+        description: "L'interior de les nostres furgonetes compta amb la tecnologia més avançada...",
+        description2: "A més, l'interior està dissenyat per oferir la màxima comoditat...",
+        icono1: "images/carousel/carousel2/no-virus.png",
+        icono2: "images/carousel/carousel2/pregnancy-test.png",
+        icono3: "images/carousel/carousel2/pacemaker.png",
+        icono4: "images/carousel/carousel2/apartment.png",
+        icono5: "images/carousel/carousel2/convenient.png"
+      },
+      {
+        contentType: "carousel3",
+        src: 'images/carousel/Merchandasing-min.png',
+        alt: "T'agrada el merchandising?",
+        title: "T'agrada el merchandising?",
+        description1: "Vols algun merchandising original? Tenim una selecció exclusiva per a tu!",
+        description2: "Des de botes personalitzades fins a tasses i bolis en forma de pipeta...",
+        icono1: "images/carousel/carousel3/cuaderno.png",
+        icono2: "images/carousel/carousel3/pipeta.png",
+        icono3: "images/carousel/carousel3/cafe.png",
+        icono4: "images/carousel/carousel3/llavero.png"
+      },
+      {
+        contentType: "carousel4",
+        src: 'images/carousel/Folleto-min.png',
+        alt: 'Oferta especial',
+        title: "Tens algún Dubte?",
+        description: 'Aprofita les nostres promocions en proves mèdiques.',
+        actionText: 'Més informació',
+        link: '/contacto'
+      }
+    ];
+
+    this.responsiveOptions = [
+      {
+        breakpoint: '1024px',
+        numVisible: 1,
+        numScroll: 1
+      },
+      {
+        breakpoint: '600px',
+        numVisible: 1,
+        numScroll: 1
+      }
+    ];
+
+  }
 
   navigate(url: string): void {
-    window.location.href = url;
+    this.router.navigate([url]);
   }
 
 }
-
